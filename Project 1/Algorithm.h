57ac0d68475f09137b8c83b831af4b123a860f2d
Matt Zanchelli <matt@mdznr.com>
2014-02-06 11:51:12 -0500

    Added header file to define MAX() and MIN() pre-processor directives.

---
+++ Project 1/Algorithm.h
@@ -0,0 +1,24 @@
+//
+//  Algorithm.h
+//  Project 1
+//
+//  Created by Matt Zanchelli on 2/6/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#ifndef Project_1_Algorithm_h
+#define Project_1_Algorithm_h
+
+/// MAX
+/// Returns the maximum of the two values.
+#ifndef MAX
+#define MAX(A,B) ( ((A) > (B)) ? (A) : (B) )
+#endif
+
+/// MIN
+/// Returns the minimum of the two values.
+#ifndef MIN
+#define MIN(A,B) ( ((A) < (B)) ? (A) : (B) )
+#endif
+
+#endif
