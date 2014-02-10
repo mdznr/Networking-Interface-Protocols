0c4ade48243a84f4d2843fec1984463fd49bb5ce
Matt Zanchelli <matt@mdznr.com>
2014-02-10 10:38:23 -0500

    Networking Layer 5 Implemented

--- Project 1/NetworkingLayer5.c
+++ Project 1/NetworkingLayer5.c
@@ -16,12 +16,95 @@
 
 int layer5_read(student *stu)
 {
-#warning layer5_read
-	return NetworkTransmissionFailure;
+	// The total number of bytes transmitted to be summed and returned.
+	int totalBytes = 0;
+	
+	// Value to hold return values for each layer 4 read.
+	int status = 0;
+	
+	// String buffer
+	char *name;
+	
+	// Fistname
+	name = malloc(sizeof(char) * 256);
+	status = layer4_read(name, 256);
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	stu->firstname = strndup(name, 256);
+	free(name);
+	
+	// Lastname
+	name = malloc(sizeof(char) * 256);
+	status = layer4_read(name, 256);
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	stu->lastname = strndup(name, 256);
+	free(name);
+	
+	// RIN
+	status = layer4_read((char *)&(stu->rin), sizeof(int));
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	
+	// GPA
+	status = layer4_read((char *)&(stu->gpa), sizeof(double));
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	
+	return totalBytes;
 }
 
 int layer5_write(student *stu)
 {
-#warning layer5_write
-	return NetworkTransmissionFailure;
+	// The total number of bytes transmitted to be summed and returned.
+	int totalBytes = 0;
+	
+	// Value to hold return values for each layer 4 write.
+	int status = 0;
+	
+	// Fistname
+	status = layer4_write(stu->firstname, sizeof(char) * (int) strnlen(stu->firstname, 256));
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	
+	// Lastname
+	status = layer4_write(stu->lastname, sizeof(char) * (int) strnlen(stu->lastname, 256));
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	
+	// RIN
+	status = layer4_write((char *)&(stu->rin), sizeof(int));
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	
+	// GPA
+	status = layer4_write((char *)&(stu->gpa), sizeof(double));
+	if ( status == NetworkTransmissionFailure ) {
+		return NetworkTransmissionFailure;
+	} else {
+		totalBytes += status;
+	}
+	
+	return totalBytes;
 }
