61fdf14eead3fb67a645055d27e9dbcc6529a610
Matt Zanchelli <matt@mdznr.com>
2014-02-11 10:42:29 -0500

    Testing helper file added

---
+++ Project 1/Testing.h
@@ -0,0 +1,26 @@
+//
+//  Testing.h
+//  Project 1
+//
+//  Created by Matt Zanchelli on 2/11/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#ifndef Project_1_Testing_h
+#define Project_1_Testing_h
+
+#include "Boolean.h"
+
+#define LINE "————————————————————————————————————————"
+
+#define PRINT_LINE printf("\n"); printf(LINE); printf("\n");
+
+/// Mark the start of a test.
+/// @param NAME The name of the test.
+#define START_TEST(NAME) PRINT_LINE; printf(NAME); printf("\n");
+
+/// Mark the end of a test.
+/// @param NAME The name of the test.
+#define END_TEST() printf("\n"); PRINT_LINE;
+
+#endif
