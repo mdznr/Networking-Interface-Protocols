f30ec623ba27d99b5a6f04d1a2335f1f11b386aa
Matt Zanchelli <matt@mdznr.com>
2014-02-11 12:39:47 -0500

    Fixing potential memory leak issues in NetworkingLayer5Tests.

--- Project 1/NetworkingLayer5Tests.c
+++ Project 1/NetworkingLayer5Tests.c
@@ -24,6 +24,19 @@ void NetworkingLayer5Tests()
 	END_TEST();
 }
 
+/// Helper function to clean free student memory.
+/// @param stu The student structure to clean up.
+void cleanupStudent(student *stu)
+{
+	if ( &(stu->firstname) != NULL ) {
+		free(stu->firstname);
+	}
+	
+	if ( &(stu->lastname) != NULL ) {
+		free(stu->lastname);
+	}
+}
+
 bool testTransmissionOfStudent()
 {
 	// Create Student.
@@ -34,31 +47,48 @@ bool testTransmissionOfStudent()
 	x.gpa = 3.81;
 	
 	int write = layer5_write(&x);
+	if ( write == NetworkTransmissionFailure ) {
+		cleanupStudent(&x);
+		return false;
+	}
 	
 	student y;
 	int read = layer5_read(&y);
-	
-	if ( strncmp(x.firstname, y.firstname, 256) != 0 ) {
-		free(x.firstname);
+	if ( read == NetworkTransmissionFailure ) {
+		cleanupStudent(&x);
+		cleanupStudent(&y);
 		return false;
-	} else {
-		free(x.firstname);
 	}
 	
-	if ( strncmp(x.lastname, y.lastname, 256) != 0 ) {
-		free(x.lastname);
+	int cmp;
+	
+	cmp = strncmp(x.firstname, y.firstname, 256);
+	if ( cmp != 0 ) {
+		cleanupStudent(&x);
+		cleanupStudent(&y);
 		return false;
-	} else {
-		free(x.lastname);
+	}
+	
+	cmp = strncmp(x.lastname, y.lastname, 256);
+	if ( cmp != 0 ) {
+		cleanupStudent(&x);
+		cleanupStudent(&y);
+		return false;
 	}
 	
 	if ( x.rin != y.rin ) {
+		cleanupStudent(&x);
+		cleanupStudent(&y);
 		return false;
 	}
 	
 	if ( x.gpa != y.gpa ) {
+		cleanupStudent(&x);
+		cleanupStudent(&y);
 		return false;
 	}
 	
+	cleanupStudent(&x);
+	cleanupStudent(&y);
 	return true;
 }
