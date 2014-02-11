6da8acdff84f1fa5b88baf72295a4b689cda1bbc
Matt Zanchelli <matt@mdznr.com>
2014-02-11 12:27:11 -0500

    Added tests for NetworkingLayer2

--- Project 1/NetworkingLayer2Tests.c
+++ Project 1/NetworkingLayer2Tests.c
@@ -10,31 +10,80 @@
 #include <string.h> // strlen
 
 #include "NetworkingLayer2.h"
+#include "Testing.h"
+
+bool test_TransmissionOfZero();
+bool test_TransmissionOfLess();
+bool test_TransmissionOfEqual();
+bool test_TransmissionOfMore();
 
 void NetworkingLayer2Tests()
 {
-	printf("\nLayer 2 Test Started\n");
-	int len;
+	START_TEST("NetworkingLayer2");
 	
-	// Test 0
-	printf("\nlayer2_write: %d\n", layer2_write(NULL, 0));
-	printf("\nlayer2_read: %d\n", layer2_read(NULL, 1));
+	test_TransmissionOfZero();
+	test_TransmissionOfLess();
+	test_TransmissionOfEqual();
+	test_TransmissionOfMore();
 	
-	// Test < max chunk size
-	char *a = "abc";
-	len = (int) strnlen(a, 256) + 1;
-	printf("\nlayer2_write: %d\n", layer2_write(a, len));
-	char b[len+1];
-	printf("layer2_read: %d\n", layer2_read(b, len));
-	printf("read: %s\n", b);
+	END_TEST();
+}
+
+bool test_TransmissionOfZero()
+{
+	int write = layer2_write(NULL, 0);
+	if ( write ) {
+		return false;
+	}
 	
-	// Test == max chunk size
+	int read = layer2_read(NULL, 1);
+	if ( read ) {
+		return false;
+	}
+	
+	return true;
+}
+
+bool test_TransmissionOfLess()
+{
+	int len = 4;
+	char *i = "abc";
+	int write = layer2_write(i, len);
+	if ( write != len ) {
+		return false;
+	}
+	
+	char o[len];
+	int read = layer2_read(o, len);
+	if ( read != len ) {
+		return false;
+	}
+	
+	if ( write != read ) {
+		return false;
+	}
+	
+	if ( strncmp(i, o, len) != 0 ) {
+		return false;
+	}
+	
+	return true;
+}
+
+bool test_TransmissionOfEqual()
+{
+	/*
 	char *c = "abcdefghijklmno";
 	len = (int) strnlen(c, 256) + 1;
 	printf("\nlayer2_write: %d\n", layer2_write(c, len));
 	char d[len+1];
 	printf("layer2_read: %d\n", layer2_read(d, len));
 	printf("read: %s", d);
-	
-	printf("\nLayer 2 Test Ended\n");
+	 */
+	return false;
+}
+
+bool test_TransmissionOfMore()
+{
+	return false;
 }
