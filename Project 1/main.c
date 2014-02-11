89520e98f5a39c1574c033900cbb33dfa12f87a5
Matt Zanchelli <matt@mdznr.com>
2014-02-10 22:35:37 -0500

    Doing a more complete test of layer 2 with three possibilities: size 0, <16, =16.

--- Project 1/main.c
+++ Project 1/main.c
@@ -109,15 +109,27 @@ void layer1Test()
 void layer2Test()
 {
 	printf("\nLayer 2 Test Started\n");
+	int len;
 	
-	// 15 chars + null terminator
-	char *b = "abcdefghijklmno";
-	int len = (int) strnlen(b, 256) + 1;
-	printf("\nlayer2_write: %d\n", layer2_write(b, len));
+	// Test 0
+	printf("\nlayer2_write: %d\n", layer2_write(NULL, 0));
+	printf("\nlayer2_read: %d\n", layer2_read(NULL, 1));
 	
-	char a[len+1];
-	printf("layer2_read: %d\n", layer2_read(a, len));
-	printf("read: %s", a);
+	// Test < max chunk size
+	char *a = "abc";
+	len = (int) strnlen(a, 256) + 1;
+	printf("\nlayer2_write: %d\n", layer2_write(a, len));
+	char b[len+1];
+	printf("layer2_read: %d\n", layer2_read(b, len));
+	printf("read: %s\n", b);
+	
+	// Test == max chunk size
+	char *c = "abcdefghijklmno";
+	len = (int) strnlen(c, 256) + 1;
+	printf("\nlayer2_write: %d\n", layer2_write(c, len));
+	char d[len+1];
+	printf("layer2_read: %d\n", layer2_read(d, len));
+	printf("read: %s", d);
 	
 	printf("\nLayer 2 Test Ended\n");
 }
